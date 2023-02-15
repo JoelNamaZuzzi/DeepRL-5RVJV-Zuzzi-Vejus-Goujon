fe5d046af80596a4db888984be9b6cb4d97e08ed
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-15 18:04:48 +0100

    Class states and actions

--- Assets/Scripts/AiUtils.cs
+++ Assets/Scripts/AiUtils.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 namespace AI_Utils
 {
 
-    public class State
+    public abstract class State
     {
         public List<Action> actions;
         public float score = 0;
         public float futureScore = 0;
         public int currentAction;
         public float reward = 0;
+    }
 
-        public State(List<Action> newActions)
+    public class Gridcase: State
+    {
+        public Gridcase()
         {
-            for(int i = 0; i < newActions.Count; i++)
-            {
-                actions.Add(newActions[i]);
-            }
+            actions.Add(new Right());
+            actions.Add(new Left());
+            actions.Add(new Down());
+            actions.Add(new Up());
 
             currentAction = Random.Range(0, actions.Count);
         }
     }
+
+    public class FinalCase: Gridcase
+    {
+        public FinalCase()
+        {
+            reward = 1;
+        }
+    }
+
+    public class Wall: Gridcase
+    {
+        public Wall()
+        {
+            reward = -100;
+        }
+    }
+
 
     public abstract class Action
     {
@@ -30,4 +50,61 @@ namespace AI_Utils
 
         public abstract Vector2Int Act(Vector2Int id);
     }
+
+    public class Right: Action
+    {
+        public override string GetId()
+        {
+            return "right";
+        }
+
+        public override Vector2Int Act(Vector2Int id)
+        {
+            id.x++;
+            return id;
+        }
+    }
+
+    public class Left: Action
+    {
+        public override string GetId()
+        {
+            return "left";
+        }
+
+        public override Vector2Int Act(Vector2Int id)
+        {
+            id.x--;
+            return id;
+        }
+    }
+
+    public class Up: Action
+    {
+        public override string GetId()
+        {
+            return "up";
+        }
+
+        public override Vector2Int Act(Vector2Int id)
+        {
+            id.y++;
+            return id;
+        }
+    }
+
+    public class Down: Action
+    {
+        public override string GetId()
+        {
+            return "down";
+        }
+
+        public override Vector2Int Act(Vector2Int id)
+        {
+            id.y--;
+            return id;
+        }
+    }
+
 }
