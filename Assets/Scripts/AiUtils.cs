3e4404b45dd117ed33ac8fc546ddf57a087469cc
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-15 15:18:40 +0100

    AI Structure v2

--- Assets/Scripts/AiUtils.cs
+++ Assets/Scripts/AiUtils.cs
@@ -8,13 +8,25 @@ namespace AI_Utils
     public class State
     {
         public List<Action> actions;
-        List<float> score = new List<float>();
+        public float score = 0;
+        public int currentAction;
+        public int futurAction;
+
+        public void State(List<Action> newActions)
+        {
+            for(int i = 0; i < newActions.Count; i++)
+            {
+                actions.Add(newActions[i]);
+            }
+
+            currentAction = Random.Range(0, actions.Count);
+        }
     }
 
     public abstract class Action
     {
         public abstract string GetId();
 
-        public abstract (int, int) Act((int, int) id);
+        public abstract Vector2 Act(Vector2 id);
     }
 }
