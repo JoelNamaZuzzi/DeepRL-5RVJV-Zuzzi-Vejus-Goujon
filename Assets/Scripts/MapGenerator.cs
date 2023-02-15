d6d5d132562f432c0ce85c293ba9a703581b1635
LTShado <56226077+LTShado@users.noreply.github.com>
2023-02-15 14:12:34 +0100

    Map change with end, start, obstacle and empty area

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,18 +7,27 @@ public class MapGenerator : MonoBehaviour
 {
     public Transform tilePrefab;
     public Vector2 mapSize;
+    private int xVal;
+    private int yVal;
+    public int[,] blocId;
+    private Transform bloc;
 
+    public List<GameObject> blocs;
     public void Start()
     {
+        xVal = (int)mapSize.x;
+        yVal = (int)mapSize.y;
+        blocId = new int[xVal, yVal];
+        
         GenerateMap();
     }
 
     public void GenerateMap()
     {
         string name = "GeneratedMap";
-        if (transform.FindChild(name))
+        if (transform.Find(name))
         {
-            DestroyImmediate(transform.FindChild(name).gameObject);
+            DestroyImmediate(transform.Find(name).gameObject);
         }
 
         Transform map = new GameObject(name).transform;
@@ -29,7 +38,29 @@ public class MapGenerator : MonoBehaviour
             for (int y = 0;y<mapSize.y;y++)
             {
                 Vector3 tilePos = new Vector3(x, 0, y);
-                Transform newTile = Instantiate(tilePrefab, tilePos, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                
+                if (x == 0 && y == 0)
+                {
+                    bloc = blocs[1].transform;
+                    blocId[x,y] = 1;
+                }
+
+                else if (x == mapSize.x - 1 && y == mapSize.y - 1)
+                {
+                    bloc = blocs[2].transform;
+                    blocId[x,y] = 2;
+                }
+                else if (x == y && x != 0 && y != 0)
+                {
+                    bloc = blocs[3].transform;
+                    blocId[x,y] = 3;
+                }
+                else
+                {
+                    bloc = blocs[0].transform;
+                    blocId[x,y] = 0;
+                }
+                Transform newTile = Instantiate(bloc, tilePos, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.parent = map;
             }
         }
