2a30cf4778f4ffba9584278bf1e44f9c3a5171cc
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2023-02-16 12:50:33 +0100

    Update BlocCrate.cs
    Just put a parent when moving crate for 1st time

--- Assets/Scripts/BlocCrate.cs
+++ Assets/Scripts/BlocCrate.cs
@@ -44,6 +44,7 @@ public class BlocCrate : Bloc
         {
             alreadyMoved = true;
             GameObject go =Instantiate(blocUnderMeGO, new Vector3(pos.x, 0, pos.y), Quaternion.Euler(Vector3.right * 90));
+            go.transform.parent = gridData[0][0].myGo.transform.parent;
             blocUnderMe.myGo = go;
         }
         //Putting the bloc under me in the grid
