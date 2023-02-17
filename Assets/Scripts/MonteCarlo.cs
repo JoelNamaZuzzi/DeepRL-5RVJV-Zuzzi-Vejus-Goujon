33a8cd8823926c9363ed6e6958b55242d5d1e011
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-17 15:02:05 +0100

    Fix montecarlo

--- Assets/Scripts/MonteCarlo.cs
+++ Assets/Scripts/MonteCarlo.cs
@@ -9,121 +9,109 @@ public class MonteCarlo : MonoBehaviour
 {
     static public void Simulation(ref Dictionary<IntList, State> mapState, int maxTurn, int nEpisode, bool everyVisit, IntList pos, float epsilon, bool onPolicy)
     {
-        // //Set to zero
-        // for(int x = 0; x < mapState.Count; x++)
-        // {
-        //     for(int y = 0; y < mapState[x].Count; y++)
-        //     {
-        //         for(int a = 0; a < mapState[x][y].actions.Count; a++)
-        //         {
-        //             mapState[x][y].totalScore[a] = 0;
-        //             mapState[x][y].timePlayed[a] = 0;
-        //         }
-        //     }
-        // }
+        //Set to zero
+        foreach(KeyValuePair<IntList, State> kvp in mapState)
+        {
+            for(int a = 0; a < kvp.Value.actions.Count; a++)
+            {
+                kvp.Value.totalScore[a] = 0;
+                kvp.Value.timePlayed[a] = 0;
+            }
+        }
 
-        // //Episodes loop
-        // for(int episode = 0; episode < nEpisode; episode++)
-        // {
-        //     //Reset visited count
-        //     for(int x = 0; x < mapState.Count; x++)
-        //     {
-        //         for(int y = 0; y < mapState[x].Count; y++)
-        //         {
-        //             for(int a = 0; a < mapState[x][y].actions.Count; a++)
-        //             {
-        //                 mapState[x][y].visited[a] = 0;
-        //             }
-        //         }
-        //     }
+        //Episodes loop
+        for(int episode = 0; episode < nEpisode; episode++)
+        {
+            //Reset visited count
+            foreach(KeyValuePair<IntList, State> kvp in mapState)
+            {
+                for(int a = 0; a < kvp.Value.actions.Count; a++)
+                {
+                    kvp.Value.visited[a] = 0;
+                }
+            }
 
-        //     List<IntList> states = new List<IntList>();
-        //     List<int> actionTaken = new List<int>();
+            List<IntList> states = new List<IntList>();
+            List<int> actionTaken = new List<int>();
 
-        //     IntList currentState = pos;
+            IntList currentState = pos;
 
-        //     //Generate an episode sequence
-        //     for(int turn = 0; turn < maxTurn; turn++)
-        //     {
+            //Generate an episode sequence
+            for(int turn = 0; turn < maxTurn; turn++)
+            {
                 
-        //         states.Add(currentState);
+                states.Add(currentState);
 
-        //         if(mapState[currentState.x][currentState.y].final == true)
-        //         {
-        //             break;
-        //         }
+                if(mapState[currentState].final == true)
+                {
+                    break;
+                }
 
-        //         //Select an action
-        //         int action = 0;
+                //Select an action
+                int action = 0;
 
-        //         //Exploration
-        //         if(Random.Range(0.0f, 1.0f) < epsilon)
-        //         {
-        //             action = Random.Range(0, mapState[currentState.x][currentState.y].actions.Count);
+                //Exploration
+                if(Random.Range(0.0f, 1.0f) < epsilon)
+                {
+                    action = Random.Range(0, mapState[currentState].actions.Count);
                     
-        //         }else{//Exploitation
-        //             action = mapState[currentState.x][currentState.y].currentAction;
-        //         }
+                }else{//Exploitation
+                    action = mapState[currentState].currentAction;
+                }
 
-        //         actionTaken.Add(action);
-        //         mapState[currentState.x][currentState.y].visited[action]++;
+                actionTaken.Add(action);
+                mapState[currentState].visited[action]++;
 
-        //         currentState = mapState[currentState.x][currentState.y].actions[action].Act(currentState);
-        //     }
+                currentState = mapState[currentState].actions[action].Act(currentState);
+            }
 
-        //     //Retropropagation
-        //     float reward = 0;
+            //Retropropagation
+            float reward = 0;
 
-        //     for(int i = states.Count-2; i >= 0; i++)
-        //     {
-        //         reward += mapState[states[i+1].x][states[i+1].y].reward;
+            for(int i = states.Count-2; i >= 0; i++)
+            {
+                reward += mapState[states[i+1]].reward;
 
-        //         if(mapState[states[i].x][states[i].y].visited[actionTaken[i]] == 1 || everyVisit == true)
-        //         {
-        //             mapState[states[i].x][states[i].y].timePlayed[actionTaken[i]]++;//Check state-action pair with teacher
-        //             mapState[states[i].x][states[i].y].totalScore[actionTaken[i]] += reward;
-        //         }
+                if(mapState[states[i]].visited[actionTaken[i]] == 1 || everyVisit == true)
+                {
+                    mapState[states[i]].timePlayed[actionTaken[i]]++;//Check state-action pair with teacher
+                    mapState[states[i]].totalScore[actionTaken[i]] += reward;
+                }
 
-        //         mapState[states[i].x][states[i].y].visited[actionTaken[i]]--;
-        //     }
+                mapState[states[i]].visited[actionTaken[i]]--;
+            }
 
-        //     if(onPolicy == true || episode == (nEpisode-1))
-        //     {
-        //         //Update V(s)
-        //         for(int x = 0; x < mapState.Count; x++)
-        //         {
-        //             for(int y = 0; y < mapState[x].Count; y++)
-        //             {
-        //                 for(int a = 0; a < mapState[x][y].actions.Count; a++)
-        //                 {
-        //                     mapState[x][y].vs[a] = mapState[x][y].totalScore[a] / mapState[x][y].timePlayed[a]; 
-        //                 }
-        //             }
-        //         }
+            if(onPolicy == true || episode == (nEpisode-1))
+            {
+                //Update V(s)
+                foreach(KeyValuePair<IntList, State> kvp in mapState)
+                {
+                    for(int a = 0; a < kvp.Value.actions.Count; a++)
+                    {
+                        kvp.Value.vs[a] = kvp.Value.totalScore[a] / kvp.Value.timePlayed[a]; 
+                    }
+                }
 
-        //         //Update policy loop
-        //         for(int x = 0; x < mapState.Count; x++)
-        //         {
-        //             for(int y = 0; y < mapState[x].Count; y++)
-        //             {
-        //                 int bestAction = 0;
-        //                 float bestScore = mapState[x][y].vs[0];
+                //Update policy loop
+                foreach(KeyValuePair<IntList, State> kvp in mapState)
+                {
+                    int bestAction = 0;
+                    float bestScore = kvp.Value.vs[0];
 
-        //                 for(int a = 1; a < mapState[x][y].actions.Count; a++)
-        //                 {
-        //                     float tmp = mapState[x][y].vs[a]; 
+                    for(int a = 1; a < kvp.Value.actions.Count; a++)
+                    {
+                        float tmp = kvp.Value.vs[a]; 
 
-        //                     if(tmp > bestScore)
-        //                     {
-        //                         bestScore = tmp;
-        //                         bestAction = a;
-        //                     }
-        //                 }
+                        if(tmp > bestScore)
+                        {
+                            bestScore = tmp;
+                            bestAction = a;
+                        }
+                    }
 
-        //                 mapState[x][y].currentAction = bestAction;
-        //             }
-        //         }
-        //     }
-        // }
+                    kvp.Value.currentAction = bestAction;
+                }
+            }
+        }
     }
 }
