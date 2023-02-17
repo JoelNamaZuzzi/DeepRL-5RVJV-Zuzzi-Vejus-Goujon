9f3ca73828b31e9f188ee4dbac107fc581a45173
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-17 13:07:37 +0100

    Map state gridworld

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,6 +5,9 @@ using System.Drawing;
 using System.Linq;
 using UnityEngine;
 
+using AI_Utils;
+using Utils;
+
 public class MapGenerator : MonoBehaviour
 {
     public Vector2Int mapSize;
@@ -14,6 +17,7 @@ public class MapGenerator : MonoBehaviour
     private Transform bloc;
     public List<GameObject> blocsPrefab;
     public List<List<Bloc>> blocs = new List<List<Bloc>>();
+    private Dictionary<IntList, State> mapState = new Dictionary<IntList, State>();
 
     [Header("Map Loading")]
     public List<Map> maps;
@@ -53,6 +57,7 @@ public class MapGenerator : MonoBehaviour
         GameObject cam = GameObject.FindGameObjectsWithTag("MainCamera")[0];
         cam.transform.position = new Vector3(xVal/2, (xVal+yVal)*0.65f , yVal/2);
     }
+
     public void GenerateMap()
     {
         string name = "GeneratedMap";
@@ -133,6 +138,91 @@ public class MapGenerator : MonoBehaviour
                 blocs[x][y].myGo.transform.position = tilePos;
                 blocs[x][y].myGo.transform.parent = map;
             }
+        }
+
+        //Generate state map
+        IntList key;
+
+        for(int x = 0; x < mapSize.x; x++)
+        {
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                State newState = new Gridcase();
+                key = new IntList();
+
+                switch(GetBlocId(new Vector2Int(x, y)))
+                {
+                    case MapGenerator.Case.Empty:
+                    case MapGenerator.Case.Start:
+                       //newState = new Gridcase();
+                       break; 
+
+                    case MapGenerator.Case.Goal:
+                       newState = new FinalGoal();
+                       break; 
+
+                    case MapGenerator.Case.Obstacle:
+                       newState = new Frobidden();
+                       break; 
+                }
+
+                key.Add(x);
+                key.Add(y);
+
+                mapState.Add(key, newState);
+            }
+        }
+
+        key = new IntList();
+        key.Add(0);
+        key.Add(0);
+
+        for(int x = 0; x < mapSize.x; x++)
+        {
+            for(int y = 0; y < mapSize.y; y++)
+            {
+                key[0] = x;
+                key[1] = y;
+
+                State currentState = mapState[key];
+
+                key[0] = x-1;
+                key[1] = y;
+
+                AddAction(key, new Left(), mapSize, currentState);
+
+                key[0] = x + 1;
+                key[1] = y;
+
+                AddAction(key, new Right(), mapSize, currentState);
+
+                key[0] = x;
+                key[1] = y - 1;
+
+                AddAction(key, new Down(), mapSize, currentState);
+
+                key[0] = x;
+                key[1] = y + 1;
+
+                AddAction(key, new Up(), mapSize, currentState);
+            }
+        }
+    }
+
+    public void AddAction(IntList key, AI_Utils.Action a, Vector2Int mapSize, State currentState)
+    {
+        //Check that everything is in bound of the map
+        for(int i = 0; i < key.Count; i+=2)
+        {
+            if(key[i] < 0 || key[i] >= mapSize.x || key[i+1] < 0 || key[i+1] >= mapSize.y)
+            {
+                return;
+            }
+        }
+
+        if(mapState[key].GetId() == "Gridcase" || mapState[key].GetId() == "StepGoal")
+        {
+            currentState.AddAction(a);
         }
     }
 }
