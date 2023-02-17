6802c783ab30a6a495fb6c679f3abef38ffec460
Mute <40270424+Ced0@users.noreply.github.com>
2023-02-17 14:54:33 +0100

    Policy iteration fix

--- Assets/Scripts/PolicyIteration.cs
+++ Assets/Scripts/PolicyIteration.cs
@@ -7,83 +7,71 @@ using Utils;
 
 public class PolicyIteration
 {
-    static void PolicyEvaluation(ref Dictionary<IntList, State> mapState, IntList mapSize, float theta, float gamma)
+    static void PolicyEvaluation(ref Dictionary<IntList, State> mapState, float theta, float gamma)
     {
-        // float delta;
+        float delta;
 
-        // do{
-        //     delta = 0;
+        do{
+            delta = 0;
 
-        //     for(int x = 0; x < mapSize.x; x++)
-        //     {
-        //         for(int x = 0; x < mapSize.x; x++)
-        //         {
-        //             for(int y = 0; y < mapSize.y; y++)
-        //             {
-        //                 IntList nextState = mapState[x][y].actions[mapState[x][y].currentAction].Act(new IntList(x, y));
+            foreach(KeyValuePair<IntList, State> kvp in mapState)
+            {
+                IntList nextState = kvp.Value.actions[kvp.Value.currentAction].Act(kvp.Key);
 
-        //                 mapState[x][y].futureScore = mapState[x][y].reward + mapState[nextState.x][nextState.y].score * gamma; 
+                kvp.Value.futureScore = kvp.Value.reward + mapState[nextState].score * gamma; 
 
-        //                 delta = Mathf.Max(delta, mapState[x][y].score - mapState[x][y].futureScore);
-        //             }
-        //         }
-        //     }
+                delta = Mathf.Max(delta, kvp.Value.score - kvp.Value.futureScore);
+            }
 
-        //     for(int x = 0; x < mapState.Count; x++)
-        //     {
-        //         for(int y = 0; y < mapState[x].Count; y++)
-        //         {
-        //             mapState[x][y].score = mapState[x][y].futureScore;
-        //         }
-        //     }
+            foreach(KeyValuePair<IntList, State> kvp in mapState)
+            {
+                kvp.Value.score = kvp.Value.futureScore;
+            }
 
-        // }while(delta >= theta);
+        }while(delta >= theta);
     }
 
     static bool PolicyImprovements(ref Dictionary<IntList, State> mapState, float gamma)
     {
         return false;
 
-        // bool stable = true;
+        bool stable = true;
 
-        // for(int x = 0; x < mapState.Count; x++)
-        // {
-        //     for(int y = 0; y < mapState[x].Count; y++)
-        //     {
-        //         bool first = true;
-        //         int bestAction = 0;
-        //         float bestScore = 0;
+        foreach(KeyValuePair<IntList, State> kvp in mapState)
+        {
+            bool first = true;
+            int bestAction = 0;
+            float bestScore = 0;
 
-        //         for(int a = 0; a < mapState[x][y].actions.Count; a++)
-        //         {
-        //             IntList nextState = mapState[x][y].actions[a].Act(new IntList(x, y));
+            for(int a = 0; a < kvp.Value.actions.Count; a++)
+            {
+                IntList nextState = kvp.Value.actions[a].Act(kvp.Key);
 
-        //             float tmp = mapState[x][y].reward + mapState[nextState.x][nextState.y].score * gamma; 
+                float tmp = kvp.Value.reward + mapState[nextState].score * gamma; 
 
-        //             if(tmp > bestScore || first == true)
-        //             {
-        //                 first = false;
-        //                 bestScore = tmp;
-        //                 bestAction = a;
-        //             }
-        //         }
+                if(tmp > bestScore || first == true)
+                {
+                    first = false;
+                    bestScore = tmp;
+                    bestAction = a;
+                }
+            }
 
-        //         if(bestAction != mapState[x][y].currentAction)
-        //         {
-        //             stable = false;
-        //         }
+            if(bestAction != kvp.Value.currentAction)
+            {
+                stable = false;
+            }
 
-        //         mapState[x][y].currentAction = bestAction;
-        //     }
-        // }
+            kvp.Value.currentAction = bestAction;
+        }
 
-        // return stable;
+        return stable;
     }
 
     static void Iteration(ref Dictionary<IntList, State> mapState, float theta, float gamma)
     {
         do{
-            //PolicyEvaluation(ref mapState, theta, gamma);
+            PolicyEvaluation(ref mapState, theta, gamma);
 
         }while(PolicyImprovements(ref mapState, gamma) == false);
     }
